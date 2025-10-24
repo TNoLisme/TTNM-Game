580e44bd921ed3341fd83229df5a37bec6fc2f11
TNoLisme <thinh01092005@gmail.com>
2025-10-24 08:34:46 +0700

    fix structure of prj after previous commit

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
+        host: 'localhost',
         port: 5173,
         proxy: {
             '/api': 'http://localhost:5000',
