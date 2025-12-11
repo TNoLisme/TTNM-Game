67f32b04ca5ef7e41d32c37ee4c59428aa94d7bf
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-12-11 23:55:59 +0700

    chỉnh logic learn.js bên user

--- be/app/main.py
+++ be/app/main.py
@@ -4,7 +4,7 @@ from app.controllers.games.game_controller import router as game_router
 from app.controllers.users.admin_controller import router as admin_router
 from app.controllers.games.cv_controller import router as cv_router
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.controllers.sessions import emotion_concepts_router
 app = FastAPI(
     title="EmoGarden API",
     description="API cho game giáo dục cảm xúc",
@@ -43,8 +43,7 @@ app.include_router(user_router)
 app.include_router(game_router)
 app.include_router(cv_router)
 app.include_router(admin_router)
-
-
+app.include_router(emotion_concepts_router)
 # CHẠY SERVER (BỎ COMMENT)
 if __name__ == "__main__":
     import uvicorn
